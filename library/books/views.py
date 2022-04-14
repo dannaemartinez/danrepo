c1d758a49a819e32e66832bb44360a361af0bd70
dannaemartinez <dannae.martinez@enroutesystems.com>
2022-04-14 21:09:15 +0000

    image sirve

--- library/books/views.py
+++ library/books/views.py
@@ -1,18 +1,25 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 from library.books.serializers import *
+import base64
+from rest_framework.response import Response
+
+class ThingViewSet(viewsets.ModelViewSet):
+    queryset = Thing.objects.all().order_by('id')
+    serializer_class = ThingSerializer
+    permission_classes = []
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all().order_by('name')
     serializer_class = AuthorSerializer
-    permission_classes = [permissions.IsAuthenticated, ]
+    permission_classes = []
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('name')
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated, ]
+    permission_classes = []
 
 class BooksAuthorsViewSet(viewsets.ModelViewSet):
     queryset = BooksAuthors.objects.all()
     serializer_class = BooksAuthorsSerializer
-    permission_classes = [permissions.IsAuthenticated, ]
+    permission_classes = []
